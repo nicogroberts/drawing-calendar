9e494a9a61c301f1b7a430e2ef1933571f43d7a1
nico <nicogroberts02@gmail.com>
2026-07-21 21:00:07 -0400

    added yearly summary

--- src/main.js
+++ src/main.js
@@ -2,3 +2,8 @@ import { displayCurrentDate, generateCalendar } from "./calendar";
 
 displayCurrentDate();
 generateCalendar();
+
+let yearlyContributions = 0;
+const mainSection = document.querySelector("main");
+const yearlySummary = document.getElementById("yearly-summary");
+yearlySummary.textContent = `${yearlyContributions} contributions in the last year`;
