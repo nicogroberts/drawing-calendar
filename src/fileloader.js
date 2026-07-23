d3d13d5ad7bbbf11d57200731879f514c173cc19
nico <nicogroberts02@gmail.com>
2026-07-22 22:03:29 -0400

    getFile now gets the text from the file

--- src/fileloader.js
+++ src/fileloader.js
@@ -1,6 +1,15 @@
 let fileHandle;
 
-export async function getFile() {
+/**
+ * Handles loading file in from file selection
+ */
+export const getFile = async () => {
     [fileHandle] = await window.showOpenFilePicker();
-    console.log(fileHandle);
+    // Get file using api then send to parser
+    if (fileHandle) {
+        const file = await fileHandle.getFile();
+        const text = await file.text();
+
+        console.log(text);
+    }
 }
