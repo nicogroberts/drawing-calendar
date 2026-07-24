cd3bfbf00932b8297726d38e2b39f7f57d9ff967
nico <nicogroberts02@gmail.com>
2026-07-23 23:12:01 -0400

    parseFile now returns an object of activity info

--- src/parser.js
+++ src/parser.js
@@ -5,21 +5,31 @@
 const parseFile = (text) => {
     console.log("Text received by parser:");
     const lines = text.split(/\r?\n/);
-    
+
+    const results = [];
+    let curr = null;
+
     lines.forEach((line) => {
+
         const trimmedLine = line.trim();
         if (trimmedLine.startsWith("Day")) {
             // Get the day number then add it to an activity object
-            let justInt = trimmedLine.replace( /[^\d.]/g, '');
-            let dayNumber = parseInt(justInt, 10);
-            console.log(dayNumber);
+            const dayNumber = parseInt(trimmedLine.replace( /[^\d.]/g, ''), 10);
+            curr = {
+                date: dayNumber,
+                time: null
+            };
         }
-        if (trimmedLine.startsWith("- Time:")) {
+        if (trimmedLine.startsWith("- Time:") && curr) {
             // Get the time then add it to an activity object
-            let activityTime = trimmedLine.match(/\d{1,2}:\d{2}:\d{2}/);
-            console.log(activityTime);
+            const timeMatch = trimmedLine.match(/\d{1,2}:\d{2}:\d{2}/);
+            const activityTime = timeMatch ? timeMatch[0] : null;
+            curr.time = activityTime;
+            results.push(curr);
         }
     });
+
+    return results;
 };
 
 export { parseFile };
